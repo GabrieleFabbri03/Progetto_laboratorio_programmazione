81ff7972c8fda65d3466ea2b00081d7afedef081
VioletHideaki <gabriele.fabbri@edu.unifi.it>
2025-02-25 20:32:47 +0000

    Creazione casuale di ostacoli e modifiche sulla dimensione della griglia

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <vector>
+#include <cstdlib>
 //creazione Griglia
 class Grid {
 private:
@@ -15,16 +16,34 @@ public:
     : gridSize(gridSize), cellSize(cellSize), start(0,0), end(gridSize - 1, gridSize - 1){
         setGridSize(gridSize);
     }
-    void setGridSize(int newSize) {
-        gridSize = newSize;
-        obstacles = std::vector<std::vector<bool>>(gridSize,std::vector<bool>(gridSize,false));
-    }
     int getGridSize() const{ return gridSize;}
     int getCellSize() const{ return cellSize;}
     const sf::Vector2i& getStart()const{ return start;}
     const sf::Vector2i& getEnd()const{ return end;}
     bool isObstacle(int x, int y)const{return obstacles[x][y];}
-//aggiorna fine mappa
+    //aggiorna fine mappa
     void updateEnd() {
         end = sf::Vector2i(gridSize - 1, gridSize - 1);
     }
+    void generateObstacles() {
+        std::srand(std::time(nullptr));
+        for (int i = 0; i < gridSize;++i)
+            for (int j = 0; j < gridSize; ++j) {
+                if ((i == start.x && j == start.y)||(i == end.x && j == end.y)) {
+                    continue;
+                }
+                obstacles[i][j] = (std::rand() % 4 == 0);
+            }
+    }
+    void setGridSize(int newSize){
+        gridSize = newSize;
+        obstacles.assign(gridSize, std::vector<bool>(gridSize, false));
+        updateEnd();
+        generateObstacles();
+    }
+};
+int main() {
+    Grid grid (10, 50);
+    std::cout<<"Griglia creata con dimensione: "<< grid.getGridSize() << "x" << grid.getGridSize() << std::endl;
+    return 0;
+}
